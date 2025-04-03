13f103941282ea92b9293a6c302f6864b73eaa4f
Mario Medina <mariomedinam@gmail.com>
2025-04-03 11:27:13 -0500

    first commit

---
+++ info-gest2/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <p className="read-the-docs">Mario Medina</p>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
