d338f7df06e1489d69414cf2d12310249bc54faa
Mario Medina <mariomedinam@gmail.com>
2025-04-03 12:04:38 -0500

    Elimina la carpeta 'info-gest2' y actualiza la configuración de Docker para el servicio 'info-gest', incluyendo ajustes en el contexto de construcción, directorio de trabajo y variables de entorno. Se añade un healthcheck y se ajustan los puertos y volúmenes. También se eliminan archivos de configuración y dependencias no necesarias.

---
+++ info-gest/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (    
+    <div className="flex flex-col items-center justify-center h-screen w-screen">      
+      <h1 class="text-3xl font-bold underline">Hello Rosy!</h1>
+    </div>
+  );
+}
+
+export default App;
